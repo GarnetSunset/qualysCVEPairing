04b137b90e57d74f796d44298919bea0e9e6b934
James Duarte <GarnetSunset@users.noreply.github.com>
2019-11-20 10:35:46 -0500

    Update qualysSchedules.py

--- qualysSchedules.py
+++ qualysSchedules.py
@@ -14,7 +14,7 @@ def qualysSchedules():
         with open('config.ini', 'r') as file:
             for line in file:
                 (key, val) = line.split('=')
-                configFile[str(key)] = val.rstrip("\n")
+                configFile[str(key)] = val.rstrip("\n").replace('"','')
             double = configFile['loginInfo'].split(':')
             username = double[0]
             password = double[1]
